630cbdff07c0761bf5c3b2ccd125d72a43a3fc02
beth nduta <bethnduta05@gmail.com>
2022-11-04 19:07:08 +0300

    update total

--- index.js
+++ index.js
@@ -44,8 +44,12 @@ function removeCartItem(event){
 
 // quantity changes
 function quantityChanged(event){
-    var input = event.target
-    
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1;
+    }
+    updatetotal();
+
 }
 
 // update total
