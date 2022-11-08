f1a927f058c3defe5ef412939861a24cfc8b0958
beth nduta <bethnduta05@gmail.com>
2022-11-08 17:56:17 +0300

    add button

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ for (var i=0; i<quantityInputs.length; i++){
 //Add to cart
 var addCart = document.getElementsByClassName('add-cart')
 for (var i=0; i<addCart.length;i++){
-    
+    var button = addCart[i];
+    button.addEventListener("click",addCartCLicked);
 }
 
 // Remove items from cart
