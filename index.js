144325aad40a52f46a897c2441ce69b1079b2230
beth nduta <bethnduta05@gmail.com>
2022-11-17 11:23:19 +0300

    add function to cart

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ function addCartCLicked(event){
     var button =event.target
     var shopProducts = button.parentElement
     var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
     console.log(title);
-    var 
 }
 
 // quantity changes
